ef110046fef295a53e4c908f7acbcfb82978cd2f
Ranur-react <rahmatnur845@gmail.com>
2025-04-11 16:27:41 +0700

    deply api simple

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 from config import get_db_connection  # import koneksi dari config
 from Corest.test import get_data,insert_data
+from  routes import API_DATA
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/api/data', methods=['GET', 'POST'])
+@app.route(API_DATA, methods=['GET', 'POST'])
 def handle_data():
     if request.method == 'GET':
         return get_data()
---
+++ routes.py
@@ -0,0 +1,11 @@
+# paths/routes_path.py
+
+# Base URI
+API_BASE = '/api'
+
+# Endpoint spesifik
+API_DATA = f'{API_BASE}/data'
+API_USER = f'{API_BASE}/user'
+API_PRODUCT = f'{API_BASE}/product'
+
+# Tambahkan endpoint lain sesuai kebutuhan
