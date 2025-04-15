3d04dd9ea3e2ade40e4a757cd9b3a06f6971b82f
Ranur-react <rahmatnur845@gmail.com>
2025-04-15 09:47:54 +0700

    koneksi ke mysql

--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 import os
 from dotenv import load_dotenv
 import pyodbc
+import mysql.connector
 
 # Load .env
 load_dotenv()
 
-# Ambil dari environment
+# Ambil dari environment untuk SQL Server
 DB_SERVER = os.getenv("DB_SERVER")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+# Ambil dari environment untuk MySQL
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
 def get_db_connection():
+    """Koneksi ke SQL Server"""
     if DB_USER and DB_PASSWORD:
         conn_str = (
             f'DRIVER={{ODBC Driver 17 for SQL Server}};'
@@ -28,3 +36,12 @@ def get_db_connection():
         )
     
     return pyodbc.connect(conn_str)
+
+def get_mysql_connection():
+    """Koneksi ke MySQL"""
+    return mysql.connector.connect(
+        host=MYSQL_HOST,
+        database=MYSQL_DATABASE,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD
+    )
